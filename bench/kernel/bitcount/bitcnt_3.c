388701d4bb480aef1d08b8fb493605865ec03aca
Yorick De Bock <yorick.debock@uantwerpen.be>
2015-10-19 17:20:15 +0200

    bitcount formatted to TACLeBench standard

--- bench/kernel/bitcount/bitcnt_3.c
+++ bench/kernel/bitcount/bitcnt_3.c
@@ -1,36 +1,26 @@
-/* +++Date last modified: 05-Jul-1997 */
-
 /*
-**  BITCNT_3.C - Bit counting functions using table lookup
-**
-**  public domain by Auke Reitsma and Bruce Wedding
+
+  This program is part of the TACLeBench benchmark suite.
+  Version V 1.x
+
+  Name: bitcnt_3.c
+
+  Author: Bob Stout & Auke Reitsma
+
+  Function:
+
+  Source:
+
+  Changes: a brief summary of major functional changes (not formatting)
+
+  License: general open-source
+
 */
 
-#include "bitops.h"   /* from Snippets */
 
-/*
-**  Bits table
-*/
+#include "bitops.h"
 
-static char bits[256] =
-  {
-    0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,   /* 0   - 15  */
-    1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,   /* 16  - 31  */
-    1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,   /* 32  - 47  */
-    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,   /* 48  - 63  */
-    1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,   /* 64  - 79  */
-    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,   /* 80  - 95  */
-    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,   /* 96  - 111 */
-    3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,   /* 112 - 127 */
-    1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,   /* 128 - 143 */
-    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,   /* 144 - 159 */
-    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,   /* 160 - 175 */
-    3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,   /* 176 - 191 */
-    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,   /* 192 - 207 */
-    3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,   /* 208 - 223 */
-    3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,   /* 224 - 239 */
-    4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8   /* 240 - 255 */
-  };
+static char bitcount_bits[256];
 
 /*
 **  Count bits in each nybble
@@ -39,17 +29,43 @@ static char bits[256] =
 **        omitted.
 */
 
-int ntbl_bitcount(long int x)
+void bitcount_init3( void )
+{
+  int i = 0;
+  char bitcount_bits_tmp[256] = {
+    0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4,
+    1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+    1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+    1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+    3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+    1, 2, 2, 3, 2, 3, 3, 4, 2, 3, 3, 4, 3, 4, 4, 5,
+    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+    3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+    2, 3, 3, 4, 3, 4, 4, 5, 3, 4, 4, 5, 4, 5, 5, 6,
+    3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+    3, 4, 4, 5, 4, 5, 5, 6, 4, 5, 5, 6, 5, 6, 6, 7,
+    4, 5, 5, 6, 5, 6, 6, 7, 5, 6, 6, 7, 6, 7, 7, 8
+  };
+
+  for ( i = 0; i < 256; i++ )
+    bitcount_bits[i] = bitcount_bits_tmp[i];
+}
+
+int bitcount_ntbl_bitcount( long int x )
 {
   return
-    bits[ (int) (x & 0x0000000FUL) ] +
-    bits[ (int)((x & 0x000000F0UL) >> 4) ] +
-    bits[ (int)((x & 0x00000F00UL) >> 8) ] +
-    bits[ (int)((x & 0x0000F000UL) >> 12)] +
-    bits[ (int)((x & 0x000F0000UL) >> 16)] +
-    bits[ (int)((x & 0x00F00000UL) >> 20)] +
-    bits[ (int)((x & 0x0F000000UL) >> 24)] +
-    bits[ (int)((x & 0xF0000000UL) >> 28)];
+    bitcount_bits[ ( int ) ( x & 0x0000000FUL ) ] +
+    bitcount_bits[ ( int )( ( x & 0x000000F0UL ) >> 4 ) ] +
+    bitcount_bits[ ( int )( ( x & 0x00000F00UL ) >> 8 ) ] +
+    bitcount_bits[ ( int )( ( x & 0x0000F000UL ) >> 12 )] +
+    bitcount_bits[ ( int )( ( x & 0x000F0000UL ) >> 16 )] +
+    bitcount_bits[ ( int )( ( x & 0x00F00000UL ) >> 20 )] +
+    bitcount_bits[ ( int )( ( x & 0x0F000000UL ) >> 24 )] +
+    bitcount_bits[ ( int )( ( x & 0xF0000000UL ) >> 28 )];
 }
 
 /*
@@ -58,18 +74,17 @@ int ntbl_bitcount(long int x)
 **  by Bruce Wedding, works best on Watcom & Borland
 */
 
-int BW_btbl_bitcount(long int x)
+int bitcount_BW_btbl_bitcount( long int x )
 {
-  union
-  {
+  union {
     unsigned char ch[4];
     long y;
   } U;
 
   U.y = x;
 
-  return bits[ U.ch[0] ] + bits[ U.ch[1] ] +
-         bits[ U.ch[3] ] + bits[ U.ch[2] ];
+  return bitcount_bits[ U.ch[0] ] + bitcount_bits[ U.ch[1] ] +
+         bitcount_bits[ U.ch[3] ] + bitcount_bits[ U.ch[2] ];
 }
 
 /*
@@ -78,14 +93,14 @@ int BW_btbl_bitcount(long int x)
 **  by Auke Reitsma, works best on Microsoft, Symantec, and others
 */
 
-int AR_btbl_bitcount(long int x)
+int bitcount_AR_btbl_bitcount( long int x )
 {
-  unsigned char * Ptr = (unsigned char *) & x ;
-  int Accu ;
+  unsigned char *ptr = ( unsigned char * ) & x ;
+  int accu ;
 
-  Accu = bits[ *Ptr++ ];
-  Accu += bits[ *Ptr++ ];
-  Accu += bits[ *Ptr++ ];
-  Accu += bits[ *Ptr ];
-  return Accu;
+  accu = bitcount_bits[ *ptr++ ];
+  accu += bitcount_bits[ *ptr++ ];
+  accu += bitcount_bits[ *ptr++ ];
+  accu += bitcount_bits[ *ptr ];
+  return accu;
 }
