d21959b1b45f01d9fc3206df95344a825da91784
Florian Kluge <kluge@informatik.uni-augsburg.de>
2015-10-20 12:34:39 +0200

    Applied TACLeBench adjustments to bitonic

--- bench/kernel/bitonic/bitonic.c
+++ bench/kernel/bitonic/bitonic.c
@@ -1,100 +1,154 @@
 /*
-* bitonic.c: C reference implementation of Bitonic Sort
-* Chris Leger <clleger@cag.lcs.mit.edu>
-* $Id: bitonic.c,v 1.2 2009-01-30 15:10:37 lokuciej Exp $
+
+  This program is part of the TACLeBench benchmark suite.
+  Version V 2.0
+
+  Name: bitonic
+
+  Author: Chris Leger
+
+  Function: bitonic implements a recursive sorting network algorithm.
+
+  Source: MiBench
+          http://wwweb.eecs.umich.edu/mibench
+
+  Changes:
+
+  License: MIT
+
 */
 
+
 /*
- * Copied almost exactly from http://www.iti.fh-flensburg.de/lang/algorithmen/sortieren/bitonic/bitonicen.htm 
- * That URL also has a nice explanation of the code and theory.
- */
+  Forward declaration of functions
+*/
+void bitonic_init( void );
+int bitonic_return( void );
+void bitonic_compare( int i, int j, int dir );
+void bitonic_merge( int lo, int cnt, int dir );
+void bitonic_sort( int lo, int cnt, int dir );
+int main( void );
 
-/* Forward declarations: */
-void compare(int i, int j, int dir);
-void bitonicMerge(int lo, int cnt, int dir);
-void bitonicSort(int lo, int cnt, int dir);
 
-/* Globals: */
-int numiters = 10;
-int a[32];         // the array to be sorted
-const int ASCENDING = 1;
-const int DESCENDING = 0;
+/*
+  Declaration of global variables
+*/
 
-int main(void)
+int bitonic_numiters = 10;
+int bitonic_a[32];         // the array to be sorted
+const int BITONIC_ASCENDING = 1;
+const int BITONIC_DESCENDING = 0;
+
+
+/*
+  Initialization- and return-value-related functions
+*/
+
+void bitonic_init( void )
 {
   /** Initialize array "a" with data **/
   int i;
-  
-  _Pragma( "loopbound min 32 max 32" )
-  for (i = 0; i < 32; i++) {
-    a[i] = (32 - i);
-  }
-  
-  /** When called with parameters lo = 0, cnt = a.length() and dir =
-  * ASCENDING, procedure bitonicSort sorts the whole array a. **/
-  _Pragma( "marker recSort" )
-  bitonicSort(0, 32, ASCENDING);
-  _Pragma( "flowrestriction 1*bitonicSort <= 63*recSort" )
 
-  /** Loop through array, printing out each element **/
   _Pragma( "loopbound min 32 max 32" )
-  for (i = 0; i < 32; i++) {
-  }
-  
-  return 0;
+  for ( i = 0; i < 32; i++ )
+    bitonic_a[i] = ( 32 - i );
 }
 
-/** A comparator is modelled by the procedure compare, where the
- * parameter dir indicates the sorting direction. If dir is ASCENDING
- * and a[i] > a[j] is true or dir is DESCENDING and a[i] > a[j] is
- * false then a[i] and a[j] are interchanged.
- **/
-void compare(int i, int j, int dir)
+
+int bitonic_return( void )
 {
-  if (dir == (a[i] > a[j])) {
-    int h = a[i];
-    a[i] = a[j];
-    a[j] = h;
+  int checksum = 0;
+
+  checksum += bitonic_a[0] + bitonic_a[21] + bitonic_a[31];
+
+  return ( checksum );
+}
+
+
+/*
+  Algorithm core functions
+*/
+
+/** A comparator is modelled by the procedure compare, where the
+   parameter dir indicates the sorting direction. If dir is ASCENDING
+   and a[i] > a[j] is true or dir is DESCENDING and a[i] > a[j] is
+   false then a[i] and a[j] are interchanged.
+ **/
+void bitonic_compare( int i, int j, int dir )
+{
+  if ( dir == ( bitonic_a[i] > bitonic_a[j] ) ) {
+    int h = bitonic_a[i];
+    bitonic_a[i] = bitonic_a[j];
+    bitonic_a[j] = h;
   }
 }
 
 /** The procedure bitonicMerge recursively sorts a bitonic sequence in
- * ascending order, if dir = ASCENDING, and in descending order
- * otherwise. The sequence to be sorted starts at index position lo,
- * the number of elements is cnt.
+   ascending order, if dir = ASCENDING, and in descending order
+   otherwise. The sequence to be sorted starts at index position lo,
+   the number of elements is cnt.
  **/
-void bitonicMerge(int lo, int cnt, int dir)
+void bitonic_merge( int lo, int cnt, int dir )
 {
   int k = cnt / 2;
   int i;
   _Pragma( "loopbound min 0 max 16" )
-  for (i = lo; i < lo + k; i++) {
-    compare(i, i + k, dir);
-  }
-    
-  if (k > 1) {
-    bitonicMerge(lo, k, dir);
-    bitonicMerge(lo + k, k, dir);
+  for ( i = lo; i < lo + k; i++ )
+    bitonic_compare( i, i + k, dir );
+
+  if ( k > 1 ) {
+    bitonic_merge( lo, k, dir );
+    bitonic_merge( lo + k, k, dir );
   }
 }
 
 /** Procedure bitonicSort first produces a bitonic sequence by
- * recursively sorting its two halves in opposite directions, and then
- * calls bitonicMerge.
+   recursively sorting its two halves in opposite directions, and then
+   calls bitonicMerge.
  **/
-void bitonicSort(int lo, int cnt, int dir)
+void bitonic_sort( int lo, int cnt, int dir )
 {
   int k = cnt;
   k /= 2;
   _Pragma( "marker recMerge" )
 
-  if (cnt > 1) {
-    bitonicSort(lo, k, ASCENDING);
-    bitonicSort(lo + k, k, DESCENDING);
+  if ( cnt > 1 ) {
+    bitonic_sort( lo, k, BITONIC_ASCENDING );
+    bitonic_sort( lo + k, k, BITONIC_DESCENDING );
   }
 
-  bitonicMerge(lo, cnt, dir);
+  bitonic_merge( lo, cnt, dir );
   _Pragma( "flowrestriction 1*bitonicMerge <= 31*recMerge" )
 
   return;
+}
+
+
+/*
+  Main functions
+*/
+
+void _Pragma ( "entrypoint" ) bitonic_main( void )
+{
+  int i;
+
+  /** When called with parameters lo = 0, cnt = a.length() and dir =
+    ASCENDING, procedure bitonicSort sorts the whole array a. **/
+  _Pragma( "marker recSort" )
+  bitonic_sort( 0, 32, BITONIC_ASCENDING );
+  _Pragma( "flowrestriction 1*bitonicSort <= 63*recSort" )
+
+  /** Loop through array, printing out each element **/
+  _Pragma( "loopbound min 32 max 32" )
+  for ( i = 0; i < 32; i++ ) {
+  }
+}
+
+
+int main( void )
+{
+  bitonic_init();
+  bitonic_main();
+
+  return ( bitonic_return() );
 }
