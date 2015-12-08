46b4735091763c6b30c1795cf0c9a6df6ea76c8c
Simon Wegener <swegener@absint.com>
2015-12-08 17:23:16 +0100

    finished to convert filterbank benchmark according to TACLeBench rules

--- bench/kernel/filterbank/filterbank.c
+++ bench/kernel/filterbank/filterbank.c
@@ -24,10 +24,10 @@
 void filterbank_init( void );
 void filterbank_main( void );
 int filterbank_return( void );
-void filterbank_core( float r[256],
-                      float y[256],
-                      float H[8][32],
-                      float F[8][32] );
+void filterbank_core( float r[ 256 ],
+                      float y[ 256 ],
+                      float H[ 8 ][ 32 ],
+                      float F[ 8 ][ 32 ] );
 
 
 /*
@@ -59,92 +59,92 @@ int filterbank_return( void )
 
 void _Pragma( "entrypoint" ) filterbank_main( void )
 {
-  float r[256];
-  float y[256];
-  float H[8][32];
-  float F[8][32];
+  float r[ 256 ];
+  float y[ 256 ];
+  float H[ 8 ][ 32 ];
+  float F[ 8 ][ 32 ];
 
   int i, j;
 
   _Pragma( "loopbound min 256 max 256" )
-  for (i = 0;i < 256;i++)
-    r[i] = i + 1;
+  for ( i = 0; i < 256; i++ )
+    r[ i ] = i + 1;
 
   _Pragma( "loopbound min 32 max 32" )
-  for (i = 0;i < 32;i++) {
+  for ( i = 0; i < 32; i++ ) {
 
     _Pragma( "loopbound min 8 max 8" )
-    for (j = 0;j < 8;j++) {
-      H[j][i] = i * 32 + j * 8 + j + i + j + 1;
-      F[j][i] = i * j + j * j + j + i;
+    for ( j = 0; j < 8; j++ ) {
+      H[ j ][ i ] = i * 32 + j * 8 + j + i + j + 1;
+      F[ j ][ i ] = i * j + j * j + j + i;
     }
   }
 
   _Pragma( "loopbound min 2 max 2" )
-  while (filterbank_numiters-- > 0) {
-    filterbank_core(r, y, H, F);
+  while ( filterbank_numiters-- > 0 ) {
+    filterbank_core( r, y, H, F );
   }
 }
 
 
 /* the FB core gets the input vector (r) , the filter responses H and F and */
 /* generates the output vector(y) */
-void filterbank_core( float r[256],
-                      float y[256],
-                      float H[8][32],
-                      float F[8][32] )
+void filterbank_core( float r[ 256 ],
+                      float y[ 256 ],
+                      float H[ 8 ][ 32 ],
+                      float F[ 8 ][ 32 ] )
 {
   int i, j, k;
 
   _Pragma( "loopbound min 256 max 256" )
-  for (i = 0; i < 256;i++)
-    y[i] = 0;
+  for ( i = 0; i < 256; i++ )
+    y[ i ] = 0;
 
   _Pragma( "loopbound min 8 max 8" )
-  for (i = 0; i < 8; i++) {
-    float Vect_H[256]; /* (output of the H) */
-    float Vect_Dn[(int) 256 / 8]; /* output of the down sampler; */
-    float Vect_Up[256]; /* output of the up sampler; */
-    float Vect_F[256]; /* this is the output of the */
+  for ( i = 0; i < 8; i++ ) {
+    float Vect_H[ 256 ]; /* (output of the H) */
+    float Vect_Dn[ (int) 256 / 8 ]; /* output of the down sampler; */
+    float Vect_Up[ 256 ]; /* output of the up sampler; */
+    float Vect_F[ 256 ]; /* this is the output of the */
 
     /* convolving H */
     _Pragma( "loopbound min 256 max 256" )
-    for (j = 0; j < 256; j++) {
-      Vect_H[j] = 0;
+    for ( j = 0; j < 256; j++ ) {
+      Vect_H[ j ] = 0;
       _Pragma( "loopbound min 1 max 32" )
-      for (k = 0; ((k < 32) & ((j - k) >= 0)); k++) {
-        Vect_H[j] += H[i][k] * r[j - k];
+      for ( k = 0; ( ( k < 32 ) & ( ( j - k ) >= 0 ) ); k++ ) {
+        Vect_H[ j ] += H[ i ][ k ] * r[ j - k ];
       }
     }
 
     /* Down Sampling */
     _Pragma( "loopbound min 32 max 32" )
-    for (j = 0; j < 256 / 8; j++)
-      Vect_Dn[j] = Vect_H[j * 8];
+    for ( j = 0; j < 256 / 8; j++ )
+      Vect_Dn[ j ] = Vect_H[ j * 8 ];
 
     /* Up Sampling */
     _Pragma( "loopbound min 256 max 256" )
-    for (j = 0; j < 256;j++)
-      Vect_Up[j] = 0;
+    for ( j = 0; j < 256; j++ )
+      Vect_Up[ j ] = 0;
     _Pragma( "loopbound min 32 max 32" )
-    for (j = 0; j < 256 / 8;j++)
-      Vect_Up[j*8] = Vect_Dn[j];
+    for ( j = 0; j < 256 / 8; j++ )
+      Vect_Up[ j*8 ] = Vect_Dn[ j ];
 
     /* convolving F */
     _Pragma( "loopbound min 256 max 256" )
-    for (j = 0; j < 256; j++) {
-      Vect_F[j] = 0;
+    for ( j = 0; j < 256; j++ ) {
+      Vect_F[ j ] = 0;
       _Pragma( "loopbound min 1 max 32" )
-      for (k = 0; ((k < 32) & ((j - k) >= 0)); k++) {
-        Vect_F[j] += F[i][k] * Vect_Up[j - k];
+      for ( k = 0; ( ( k < 32 ) & ( ( j - k ) >= 0 ) ); k++ ) {
+        Vect_F[ j ] += F[ i ][ k ] * Vect_Up[ j - k ];
       }
     }
 
     /* adding the results to the y matrix */
 
     _Pragma( "loopbound min 256 max 256" )
-    for (j = 0; j < 256; j++) {
-      y[j] += Vect_F[j];
+    for ( j = 0; j < 256; j++ ) {
+      y[ j ] += Vect_F[ j ];
     }
   }
 }
