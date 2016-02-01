f2ba9c552a2ae5350397dd935d015bfc2261a0fd
Florian Kluge <kluge@informatik.uni-augsburg.de>
2016-02-01 16:44:28 +0100

    Adjusted select according to TACLeBench guidelines

--- bench/kernel/select/select.c
+++ bench/kernel/select/select.c
@@ -1,115 +1,171 @@
-/*************************************************************************/
-/*                                                                       */
-/*   SNU-RT Benchmark Suite for Worst Case Timing Analysis               */
-/*   =====================================================               */
-/*                              Collected and Modified by S.-S. Lim      */
-/*                                           sslim@archi.snu.ac.kr       */
-/*                                         Real-Time Research Group      */
-/*                                        Seoul National University      */
-/*                                                                       */
-/*                                                                       */
-/*        < Features > - restrictions for our experimental environment   */
-/*                                                                       */
-/*          1. Completely structured.                                    */
-/*               - There are no unconditional jumps.                     */
-/*               - There are no exit from loop bodies.                   */
-/*                 (There are no 'break' or 'return' in loop bodies)     */
-/*          2. No 'switch' statements.                                   */
-/*          3. No 'do..while' statements.                                */
-/*          4. Expressions are restricted.                               */
-/*               - There are no multiple expressions joined by 'or',     */
-/*                'and' operations.                                      */
-/*          5. No library calls.                                         */
-/*               - All the functions needed are implemented in the       */
-/*                 source file.                                          */
-/*                                                                       */
-/*                                                                       */
-/*************************************************************************/
-/*                                                                       */
-/*  FILE: select.c                                                       */
-/*  SOURCE : Numerical Recipes in C - The Second Edition                 */
-/*                                                                       */
-/*  DESCRIPTION :                                                        */
-/*                                                                       */
-/*     A function to select the Nth largest number in the floating poi-  */
-/*     nt array arr[].                                                   */
-/*     The parameters to function select are k and n. Then the function  */
-/*     selects k-th largest number out of n original numbers.            */
-/*                                                                       */
-/*  REMARK :                                                             */
-/*                                                                       */
-/*  EXECUTION TIME :                                                     */
-/*                                                                       */
-/*                                                                       */
-/*************************************************************************/
-#define SWAP(a,b) temp=(a);(a)=(b);(b)=temp;
+/*
+
+  This program is part of the TACLeBench benchmark suite.
+  Version V 2.0
+
+  Name:
+
+  Author: William H. Press, Saul A. Teukolsky, William T. Vetterling,
+          Brian P. Flannery, "Numerical Recipes in C"
+
+  Function: A function to select the Nth largest number in the floating
+            point array arr[].
+            The parameters to function select are k and n. Then the function
+            selects k-th largest number out of n original numbers.
+
+  Source: SNU-RT Benchmark Suite for Worst Case Timing Analysis
+          Collected and Modified by S.-S. Lim
+          Real-Time Research Group
+          Seoul National University
+
+  Changes: no major functional changes
+
+  License: unknown
+
+*/
+
+/*
+  The original algorithm was adjusted in the SNU-RT Benchmark Suite:
+  < Features > - restrictions for our experimental environment   
+  
+  1. Completely structured.
+      - There are no unconditional jumps.
+      - There are no exit from loop bodies.
+      (There are no 'break' or 'return' in loop bodies)
+  2. No 'switch' statements.
+  3. No 'do..while' statements.
+  4. Expressions are restricted.
+      - There are no multiple expressions joined by 'or',
+        'and' operations.
+  5. No library calls.
+      - All the functions needed are implemented in the
+        source file.
+*/
+
+/*
+  Forward declaration of functions
+*/
+void select_init( void );
+float select_return( void );
+int select_main( void );
+int main( void );
 
 
-static float select(unsigned long k, unsigned long n)
+/*
+  Global variables
+*/
+
+float select_selected = 0.0;
+float select_arr[ 20 ];
+
+
+#define SWAP(a,b) { temp = (a); (a) = (b); (b) = temp; }
+
+
+void select_init( void )
 {
-  float arr[20] = {
-    5.0f,   4.0f,  10.3f,   1.1f,   5.7f,
-    100.0f, 231.0f, 111.0f,  49.5f,  99.0f,
-    10.0f, 150.0f, 222.22f, 101.0f, 77.0f,
-    44.0f,  35.0f,  20.54f, 99.99f, 888.88f
-  };
+  select_arr[ 0 ] = 5.0f;
+  select_arr[ 1 ] = 4.0f;
+  select_arr[ 2 ] = 10.3f;
+  select_arr[ 3 ] = 1.1f;
+  select_arr[ 4 ] = 5.7f;
+  select_arr[ 5 ] = 100.0f;
+  select_arr[ 6 ] = 231.0f;
+  select_arr[ 7 ] = 111.0f;
+  select_arr[ 8 ] = 49.5f;
+  select_arr[ 9 ] = 99.0f;
+  select_arr[ 10 ] = 10.0f;
+  select_arr[ 11 ] = 150.0f;
+  select_arr[ 12 ] = 222.22f;
+  select_arr[ 13 ] = 101.0f;
+  select_arr[ 14 ] = 77.0f;
+  select_arr[ 15 ] = 44.0f;
+  select_arr[ 16 ] = 35.0f;
+  select_arr[ 17 ] = 20.54f;
+  select_arr[ 18 ] = 99.99f;
+  select_arr[ 19 ] = 888.88f;
+}
 
-  unsigned long i,ir,j,l,mid;
-  float a,temp;
+
+float select_return( void )
+{
+  return select_selected;
+}
+
+
+static float select_select( unsigned long k, unsigned long n )
+{
+
+  unsigned long i, ir, j, l, mid;
+  float a, temp;
   int flag, flag2;
 
-  l=1;
-  ir=n;
+  l = 0;
+  ir = n - 1;
   flag = flag2 = 0;
-  _Pragma("loopbound min 16 max 16")
-  while (!flag) {
-    if (ir <= l+1) {
-      if (ir == l+1)
-        if (arr[ir] < arr[l]) {
-          SWAP(arr[l],arr[ir])
-            }
+  _Pragma( "loopbound min 16 max 16" )
+  while ( !flag ) {
+    if ( ir <= l + 1 ) {
+      if ( ir == l + 1 )
+        if ( select_arr[ ir ] < select_arr[ l ] ) {
+          SWAP( select_arr[ l ], select_arr[ ir ] )
+        }
       flag = 1;
-    } else if (!flag) {
-      mid=(l+ir) >> 1;
-      SWAP(arr[mid],arr[l+1])
-        if (arr[l+1] > arr[ir]) {
-          SWAP(arr[l+1],arr[ir])
-            }
-      if (arr[l] > arr[ir]) {
-        SWAP(arr[l],arr[ir])
-          }
-      if (arr[l+1]> arr[l]) {
-        SWAP(arr[l+1],arr[l])
-          }
-      i=l+1;
-      j=ir;
-      a=arr[l];
-      _Pragma("loopbound min 0 max 1")
-      while (!flag2) {
-        i++;
-	_Pragma("loopbound min 1 max 1")
-        while (arr[i] < a) i++;
-        j--;
-	_Pragma("loopbound min 16 max 16")
-        while (arr[j] > a) j--;
-        if (j < i) flag2 = 1;
-        if (!flag2) SWAP(arr[i],arr[j]);
+    } else {
+      if ( !flag ) {
+        mid = ( l + ir ) >> 1;
+        SWAP( select_arr[ mid ], select_arr[ l + 1 ] )
+        if ( select_arr[ l ] > select_arr[ ir ] ) {
+          SWAP( select_arr[ l ], select_arr[ ir ] )
+        }
+        if ( select_arr[ l + 1 ] > select_arr[ ir ] ) {
+          SWAP( select_arr[ l + 1 ], select_arr[ ir ] )
+        }
+        if ( select_arr[ l ] > select_arr[ l + 1 ] ) {
+          SWAP( select_arr[ l ], select_arr[ l + 1 ] )
+        }
+        i = l + 1;
+        j = ir;
+        a = select_arr[ l + 1 ];
+        _Pragma( "loopbound min 0 max 1" )
+	flag2 = 0;
+        while ( !flag2 ) {
+          i++;
+          _Pragma( "loopbound min 1 max 1" )
+	  while ( select_arr[ i ] < a ) i++;
+          j--;
+          _Pragma( "loopbound min 16 max 16" )
+	  while ( select_arr[ j ] > a ) j--;
+          if ( j < i ) flag2 = 1;
+          if ( !flag2 ) {
+	    SWAP( select_arr[ i ], select_arr[ j ] );
+	  }
 
+        }
+        select_arr[ l + 1 ] = select_arr[ j ];
+        select_arr[ j ] = a;
+
+        if ( j >= k ) ir = j - 1;
+        if ( j <= k ) l = i;
       }
-      arr[l]=arr[j];
-      arr[j]=a;
-
-      if (j >= k) ir=j-1;
-      if (j <= k) l=i;
     }
-
   }
-  return arr[k];
+  return select_arr[ k ];
 }
+
+int _Pragma ( "entrypoint" ) select_main()
+{
+  select_selected = select_select( 7, 20 );
+  return 0;
+}
+
+#include <stdio.h>
 
 int main()
 {
-  select(10, 20);
-  return 0;
+  select_init();
+  select_main();
+
+  return ( select_return() );
 }
 
