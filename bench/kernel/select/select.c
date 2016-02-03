d297bc0d7a1dfeecf812643590ef0ac41a3e5b68
Florian Kluge <kluge@informatik.uni-augsburg.de>
2016-02-03 14:06:22 +0100

    Revision of select: SWAP macro and loop bounds

--- bench/kernel/select/select.c
+++ bench/kernel/select/select.c
@@ -13,6 +13,24 @@
             The parameters to function select are k and n. Then the function
             selects k-th largest number out of n original numbers.
 
+	    The original algorithm was adjusted for inclusion in the
+	    SNU-RT Benchmark Suite:
+
+	    < Features > - restrictions for our experimental environment   
+  
+	    1. Completely structured.
+	      - There are no unconditional jumps.
+	      - There are no exit from loop bodies.
+	      (There are no 'break' or 'return' in loop bodies)
+	    2. No 'switch' statements.
+	    3. No 'do..while' statements.
+	    4. Expressions are restricted.
+	      - There are no multiple expressions joined by 'or',
+	      'and' operations.
+	    5. No library calls.
+	      - All the functions needed are implemented in the
+	      source file.
+	    
   Source: SNU-RT Benchmark Suite for Worst Case Timing Analysis
           Collected and Modified by S.-S. Lim
           Real-Time Research Group
@@ -22,24 +40,6 @@
 
   License: unknown
 
-*/
-
-/*
-  The original algorithm was adjusted in the SNU-RT Benchmark Suite:
-  < Features > - restrictions for our experimental environment   
-  
-  1. Completely structured.
-      - There are no unconditional jumps.
-      - There are no exit from loop bodies.
-      (There are no 'break' or 'return' in loop bodies)
-  2. No 'switch' statements.
-  3. No 'do..while' statements.
-  4. Expressions are restricted.
-      - There are no multiple expressions joined by 'or',
-        'and' operations.
-  5. No library calls.
-      - All the functions needed are implemented in the
-        source file.
 */
 
 /*
@@ -59,7 +59,7 @@ float select_selected = 0.0;
 float select_arr[ 20 ];
 
 
-#define SWAP(a,b) { temp = (a); (a) = (b); (b) = temp; }
+#define SWAP(a,b) do { temp = (a); (a) = (b); (b) = temp; } while (0)
 
 
 void select_init( void )
@@ -103,38 +103,38 @@ static float select_select( unsigned long k, unsigned long n )
   l = 0;
   ir = n - 1;
   flag = flag2 = 0;
-  _Pragma( "loopbound min 16 max 16" )
+  _Pragma( "loopbound min 1 max 20" )
   while ( !flag ) {
     if ( ir <= l + 1 ) {
       if ( ir == l + 1 )
         if ( select_arr[ ir ] < select_arr[ l ] ) {
-          SWAP( select_arr[ l ], select_arr[ ir ] )
+          SWAP( select_arr[ l ], select_arr[ ir ] );
         }
       flag = 1;
     } else {
       if ( !flag ) {
         mid = ( l + ir ) >> 1;
-        SWAP( select_arr[ mid ], select_arr[ l + 1 ] )
+        SWAP( select_arr[ mid ], select_arr[ l + 1 ] );
         if ( select_arr[ l ] > select_arr[ ir ] ) {
-          SWAP( select_arr[ l ], select_arr[ ir ] )
+          SWAP( select_arr[ l ], select_arr[ ir ] );
         }
         if ( select_arr[ l + 1 ] > select_arr[ ir ] ) {
-          SWAP( select_arr[ l + 1 ], select_arr[ ir ] )
+          SWAP( select_arr[ l + 1 ], select_arr[ ir ] );
         }
         if ( select_arr[ l ] > select_arr[ l + 1 ] ) {
-          SWAP( select_arr[ l ], select_arr[ l + 1 ] )
+          SWAP( select_arr[ l ], select_arr[ l + 1 ] );
         }
         i = l + 1;
         j = ir;
         a = select_arr[ l + 1 ];
-        _Pragma( "loopbound min 0 max 1" )
+        _Pragma( "loopbound min 1 max 9" )
 	flag2 = 0;
         while ( !flag2 ) {
           i++;
-          _Pragma( "loopbound min 1 max 1" )
+          _Pragma( "loopbound min 0 max 18" )
 	  while ( select_arr[ i ] < a ) i++;
           j--;
-          _Pragma( "loopbound min 16 max 16" )
+          _Pragma( "loopbound min 0 max 18" )
 	  while ( select_arr[ j ] > a ) j--;
           if ( j < i ) flag2 = 1;
           if ( !flag2 ) {
