fc4e23ecba9cc9a9b57e0eef656ef5de1d4eadf2
Simon Wegener <swegener@absint.com>
2016-03-15 20:34:19 +0100

    applied astyle

--- bench/kernel/bsort100/bsort100.c
+++ bench/kernel/bsort100/bsort100.c
@@ -1,9 +1,9 @@
-/* 
- * DESCRIPTION: Bubblesort program
- *
- * COMMENTS: Tests the basic loop constructs, integer comparisons, and simple
- * 	     array handling by sorting 100 integers
- */
+/*
+   DESCRIPTION: Bubblesort program
+
+   COMMENTS: Tests the basic loop constructs, integer comparisons, and simple
+         array handling by sorting 100 integers
+*/
 
 /* A read from this address will result in an known value of 1 */
 //#define KNOWN_VALUE (int)(*((char *)0x80200001))
@@ -17,10 +17,10 @@
 
 
 /* BUBBLESORT BENCHMARK PROGRAM:
- * This program tests the basic loop constructs, integer comparisons,
- * and simple array handling of compilers by sorting 10 arrays of
- * randomly generated integers.
- */
+   This program tests the basic loop constructs, integer comparisons,
+   and simple array handling of compilers by sorting 10 arrays of
+   randomly generated integers.
+*/
 
 int Array[MAXDIM], Seed;
 int factor;
@@ -33,63 +33,62 @@ int BubbleSort( int Array[] );
 int main( void )
 {
   Initialize( Array );
-  
-  BubbleSort(Array);
-  
+
+  BubbleSort( Array );
+
   return 0;
 }
 
 
 int Initialize( int Array[] )
 /*
- * Initializes given array with randomly generated integers.
- */
+   Initializes given array with randomly generated integers.
+*/
 {
   int  Index, fact;
 
-#ifdef WORSTCASE
+  #ifdef WORSTCASE
   factor = -1;
-#else
+  #else
   factor = 1;
-#endif
+  #endif
 
   fact = factor;
 
-  _Pragma("loopbound min 100 max 100")
-  for ( Index = 1; Index <= NUMELEMS; Index++ ) {
-      Array[Index] = Index*fact * KNOWN_VALUE;
-  }
+  _Pragma( "loopbound min 100 max 100" )
+  for ( Index = 1; Index <= NUMELEMS; Index++ )
+    Array[Index] = Index * fact * KNOWN_VALUE;
 
-  return 0; 
+  return 0;
 }
 
 
 
 int BubbleSort( int Array[] )
 /*
- * Sorts an array of integers of size NUMELEMS in ascending order.
- */
+   Sorts an array of integers of size NUMELEMS in ascending order.
+*/
 {
   int Sorted = FALSE;
   int Temp, LastIndex, Index, i;
 
-  _Pragma("loopbound min 99 max 99")
-  for ( i = 1; i <= NUMELEMS-1; i++ ) {
+  _Pragma( "loopbound min 99 max 99" )
+  for ( i = 1; i <= NUMELEMS - 1; i++ ) {
     Sorted = TRUE;
-    _Pragma("loopbound min 2 max 99")
-    for (Index = 1; Index <= NUMELEMS-1; Index ++ ) {
-      if ( Index > NUMELEMS-i )
+    _Pragma( "loopbound min 2 max 99" )
+    for ( Index = 1; Index <= NUMELEMS - 1; Index ++ ) {
+      if ( Index > NUMELEMS - i )
         break;
       if ( Array[Index] > Array[Index + 1] ) {
         Temp = Array[Index];
-        Array[Index] = Array[Index+1];
-        Array[Index+1] = Temp;
+        Array[Index] = Array[Index + 1];
+        Array[Index + 1] = Temp;
         Sorted = FALSE;
       }
     }
 
-    if (Sorted)
-     break;
+    if ( Sorted )
+      break;
   }
 
   return 0;
