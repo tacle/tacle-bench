330bf2c95b3bb1fcfa80040029594afea367cd18
Simon Wegener <swegener@absint.com>
2016-03-15 22:14:54 +0100

    formatted benchmark according to TACLeBench coding style guidelines

--- bench/kernel/bsort100/bsort100.c
+++ bench/kernel/bsort100/bsort100.c
@@ -7,8 +7,9 @@
 
   Author: unknown
 
-  Function: A program for testing the basic loop constructs, integer
-            comparisons, and simple array handling by sorting 100 integers
+  Function: A program for testing the basic loop constructs,
+            integer comparisons, and simple array handling by
+            sorting 100 integers
 
   Source: MRTC
           http://www.mrtc.mdh.se/projects/wcet/wcet_bench/bsort100/bsort100.c
@@ -39,7 +40,7 @@ int bsort_BubbleSort( int Array[] );
 
 #define bsort_SIZE 100
 
-static int bsort_Array[bsort_SIZE];
+static int bsort_Array[ bsort_SIZE ];
 
 
 /*
@@ -49,11 +50,11 @@ static int bsort_Array[bsort_SIZE];
 /* Initializes given array with randomly generated integers. */
 int bsort_Initialize( int Array[] )
 {
-  int  Index;
+  int Index;
 
   _Pragma( "loopbound min 100 max 100" )
-  for ( Index = 0; Index < bsort_SIZE; Index++ )
-    Array[Index] = ( Index + 1 ) * -1;
+  for ( Index = 0; Index < bsort_SIZE; Index ++ )
+    Array[ Index ] = ( Index + 1 ) * -1;
 
   return 0;
 }
@@ -83,16 +84,16 @@ int bsort_BubbleSort( int Array[] )
   int Temp, Index, i;
 
   _Pragma( "loopbound min 99 max 99" )
-  for ( i = 0; i < bsort_SIZE - 1; i++ ) {
+  for ( i = 0; i < bsort_SIZE - 1; i ++ ) {
     Sorted = 1;
     _Pragma( "loopbound min 2 max 99" )
     for ( Index = 0; Index < bsort_SIZE - 1; Index ++ ) {
       if ( Index > bsort_SIZE - i )
         break;
-      if ( Array[Index] > Array[Index + 1] ) {
-        Temp = Array[Index];
-        Array[Index] = Array[Index + 1];
-        Array[Index + 1] = Temp;
+      if ( Array[ Index ] > Array[Index + 1] ) {
+        Temp = Array[ Index ];
+        Array[ Index ] = Array[ Index + 1 ];
+        Array[ Index + 1 ] = Temp;
         Sorted = 0;
       }
     }
