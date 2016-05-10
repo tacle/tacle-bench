975522f574a0b18fb71fa1814daa4883712ed180
Peter Waegemann <waegemann@cs.fau.de>
2016-05-10 15:46:30 +0200

    Fix bug in gsm_dec: avoid shifting negative signed value

--- bench/sequential/gsm_dec/gsm.h
+++ bench/sequential/gsm_dec/gsm.h
@@ -2,13 +2,13 @@
 #define GSM_DEC_GSM_H
 
 /*
- *	Interface
- */
-typedef struct gsm_state * 	gsm;
-typedef short		   	gsm_signal;		/* signed 16 bit */
-typedef unsigned char		gsm_byte;
-typedef gsm_byte 		gsm_frame[33];		/* 33 * 8 bits	 */
+    Interface
+*/
+typedef struct gsm_state *gsm;
+typedef short gsm_signal; /* signed 16 bit */
+typedef unsigned char gsm_byte;
+typedef gsm_byte gsm_frame[33]; /* 33 * 8 bits   */
 
-#define	GSM_MAGIC	0xD			  	/* 13 kbit/s RPE-LTP */
+#define GSM_MAGIC 0xD /* 13 kbit/s RPE-LTP */
 
-#endif	/* GSM_DEC_GSM_H */
+#endif /* GSM_DEC_GSM_H */
